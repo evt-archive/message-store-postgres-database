adb54090acaa288bdaf3813fc75e6500600771ef
Scott Bellware <sbellware@gmail.com>
2019-10-13 23:47:49 -0500

    Ruby infrastructure is removed

--- init.rb
+++
@@ -1,3 +0,0 @@
-require_relative './load_path'
-
-require 'postgres_message_store'
--- test/automated/print_messages.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'automated_init'
-
-context "Print Messages" do
-  script_file = 'scripts/evt-pg-print-messages'
-
-  system script_file
-end
